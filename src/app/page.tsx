75ea73a2e046dc72c189d638ddd73dbfe538d969
darwishathibi <danish.darwis@live.iium.edu.my>
2025-04-16 22:07:13 +0800

    implement lenis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import IntroPage from "./Intro";
 import ProjectPage from "./Project";
 import SkillsPage from "./Skills";
 import Footer from "./Footer";
 import ContactPage from "./Contact";
-import React from "react";
+import React, { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <div className="flex flex-col ">
       <IntroPage />
